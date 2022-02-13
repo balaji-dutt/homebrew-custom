8df2ead8bc4ba923c377bc7f1aa0e2561d34520a
Balaji Dutt <gitlab@dutt.me>
2022-02-13 15:47:50 +0800

    Add kube-cli@1.23.3 and upgrade kubectx dependency.

--- Formula/kubectx@0.9.4.rb
+++ Formula/kubectx@0.9.4.rb
@@ -6,7 +6,7 @@ class KubectxAT094 < Formula
   license "Apache-2.0"
   head "https://github.com/ahmetb/kubectx.git"
 
-  depends_on "balaji-dutt/custom/kubernetes-cli@1.23.0"
+  depends_on "balaji-dutt/custom/kubernetes-cli@1.23.3"
 
   def install
     bin.install "kubectx", "kubens"
